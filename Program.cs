2b751bc2a22c4b5536b023430c73b7bc6e62b358
Ermogenes Palacio <ermogenes@gmail.com>
2020-09-04 20:35:40 -0300

    Resolução do exercício

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace etecab_2020_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Etec Adolpho Berezin");
+        }
+    }
+}
